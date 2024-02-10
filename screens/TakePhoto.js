bced199bfc0490eb31ea0bbae01f401c2137904c
Sebastian-Webster <84299475+Sebastian-Webster@users.noreply.github.com>
2024-02-10 15:15:45 +1300

    Added flash and drop shadow for UI elements

--- screens/TakePhoto.js
+++ screens/TakePhoto.js
@@ -15,6 +15,7 @@ const TakePhoto = () => {
     const focused = useIsFocused();
     const camera = useRef();
     const [takenPhotoToday, setTakenPhotoToday] = useState(null)
+    const [flashOn, setFlashOn] = useState(false);
     const [chosenCamera, setChosenCamera] = useState('back')
     const { colors } = useTheme()
     const frontAndBackEnabled = frontDevice && backDevice ? true : false;
@@ -81,7 +82,7 @@ const TakePhoto = () => {
 
     const takePhoto = async () => {
         const photo = await camera.current.takePhoto({
-            flash: 'off',
+            flash: !device.hasFlash ? 'off' : flashOn ? 'on' : 'off',
             qualityPrioritization: 'speed'
         })
 
@@ -141,11 +142,16 @@ const TakePhoto = () => {
                             <>
                                 <View style={{position: 'absolute', zIndex: 2, top: 0, flexDirection: 'row', justifyContent: 'center', alignItems: 'flex-end', height: '100%', width: '100%', paddingBottom: 20}}>
                                     {frontAndBackEnabled && (
-                                        <TouchableOpacity onPress={changeCamera} style={{position: 'absolute', left: 10, bottom: 20}}>
+                                        <TouchableOpacity onPress={changeCamera} style={[{position: 'absolute', left: 10, bottom: 20}, styles.dropShadow]}>
                                             <Ionicons name="camera-reverse-sharp" size={70} color="white"/>
                                         </TouchableOpacity>
                                     )}
-                                    <TouchableOpacity onPress={takePhoto} style={{backgroundColor: 'black', width: 70, height: 70, borderColor: 'white', borderWidth: 2, borderRadius: 1000}}></TouchableOpacity>
+                                    {device.hasFlash && (
+                                        <TouchableOpacity onPress={() => setFlashOn(flashOn => !flashOn)} style={[{position: 'absolute', right: 10, bottom: 20}, styles.dropShadow]}>
+                                            <Ionicons name={flashOn ? 'flash' : 'flash-off'} size={70} color="white"/>
+                                        </TouchableOpacity>
+                                    )}
+                                    <TouchableOpacity onPress={takePhoto} style={[{backgroundColor: 'black', width: 70, height: 70, borderColor: 'white', borderWidth: 2, borderRadius: 1000}, styles.dropShadow]}></TouchableOpacity>
                                 </View>
                                 <Camera
                                     style={{height: '100%', width: '100%'}}
@@ -181,5 +187,15 @@ const TakePhoto = () => {
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    dropShadow: {
+        elevation: 20,
+        shadowColor: 'black',
+        shadowOffset: {width: 1, height: 1},
+        shadowOpacity: 0.6,
+        shadowRadius: 10
+    }
+})
 
 export default TakePhoto;
