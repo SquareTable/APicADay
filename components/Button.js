3cfcd6d078a6f91d1c0c618f9de1cf92d9aca1e5
Sebastian-Webster <84299475+Sebastian-Webster@users.noreply.github.com>
2024-02-13 14:30:12 +1300

    fix(v0.0.2): end date can no longer be greater than start date

--- components/Button.js
+++ components/Button.js
@@ -1,11 +1,11 @@
 import { useTheme } from "@react-navigation/native";
 import { Text, TouchableOpacity } from "react-native";
 
-export default function Button({onPress, text, style = {}, textStyle = {}, disabled = false}) {
+export default function Button({onPress, text, style = {}, textStyle = {}, disabled = false, active = true}) {
     const {colors} = useTheme();
 
     return (
-        <TouchableOpacity disabled={disabled} onPress={onPress} style={{borderWidth: 1, borderColor: colors.text, paddingHorizontal: 20, paddingVertical: 10, borderRadius: 10, marginTop: 10, ...style}}>
+        <TouchableOpacity disabled={disabled} onPress={onPress} style={{borderWidth: 1, borderColor: colors.text, paddingHorizontal: 20, paddingVertical: 10, borderRadius: 10, marginTop: 10, opacity: active ? 1 : 0.6, ...style}}>
             <Text style={{fontSize: 20, fontWeight: 'bold', color: colors.text, ...textStyle}}>{text}</Text>
         </TouchableOpacity>
     )
